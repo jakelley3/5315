85c7a2876c5ab0c2f7d7691aa886e9791ac915f2
jake <kelleyj2@seattleu.edu>
2019-04-28 10:37:34 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -5,105 +5,120 @@
       "designation": "Project Manager",
       "salary": "$550,000",
       "joining_date": "2012/08/26",
-
+      "office": "SLU",
+      "extension": "2500"
     },
     {
       "name": "Spencer Woods",
       "designation": "Sales",
       "salary": "$50,000",
       "joining_date": "2012/12/02",
-
+      "office": "Seattle",
+      "extension": "5000"
     },
     {
       "name": "Jeff leonard",
       "designation": "Financial Analyst",
       "salary": "$100,000",
       "joining_date": "2009/01/12",
-  
+      "office": "brazil",
+      "extension": "2002"
     },
     {
       "name": "Jay berry",
       "designation": "baseball Player",
       "salary": "$6,000,000",
       "joining_date": "2008/11/28",
-
+      "office": "Texas",
+      "extension": "5000"
     },
     {
       "name": "Jay Dub",
       "designation": "Football Player",
       "salary": "$200,450",
       "joining_date": "2011/12/12",
-
+      "office": "Mexico",
+      "extension": "80"
     },
     {
       "name": "Russel Wilson",
       "designation": "Football Player",
       "salary": "$183,000,000",
       "joining_date": "2008/11/13",
- 
+      "office": "Seattle",
+      "extension": "425"
     },
     {
       "name": "Barry bonds",
       "designation": "baseball Player",
       "salary": "$1,000,000,000",
       "joining_date": "2012/03/29",
-   
+      "office": "San Francisco",
+      "extension": "360"
     },
     {
       "name": "Tiger Woods",
       "designation": "Golfer",
       "salary": "$513,000,000",
       "joining_date": "2012/12/18",
-   
+      "office": "Florida",
+      "extension": "206"
     },
     {
       "name": "Scott Bowman",
       "designation": "Javascript Developer",
       "salary": "$305,500,000",
       "joining_date": "2009/09/15",
- 
+      "office": "San Francisco",
+      "extension": "23"
     },
     {
       "name": "Ichiro Suzuki",
       "designation": "Baseball Player",
       "salary": "$100,500",
       "joining_date": "2012/09/26",
-
+      "office": "Japan",
+      "extension": "90"
     },
     {
       "name": "Tom Brady",
       "designation": "Football Player",
       "salary": "$250,000,000",
       "joining_date": "2012/08/06",
- 
+      "office": "New England",
+      "extension": "9608"
     },
     {
       "name": "Marsahwn Lynch",
       "designation": "Football Player",
       "salary": "$500,000",
       "joining_date": "2010/10/14",
-   
+      "office": "Seattke",
+      "extension": "206"
     },
     {
       "name": "Richard Sherman",
       "designation": "Football Player",
       "salary": "$100,000",
       "joining_date": "2008/12/13",
-      
+      "office": "Seattle",
+      "extension": "206"
     },
     {
       "name": "Bobby Wagner",
       "designation": "Football Player",
       "salary": "$900,000",
       "joining_date": "2008/12/19",
-      
+      "office": "Seattle",
+      "extension": "206"
     },
     {
       "name": "Lebron James",
       "designation": "Basketball Player",
       "salary": "$100,000,000,000",
       "joining_date": "2013/03/03",
-      
+      "office": "Los Angeles",
+      "extension": "100"
     }
   ]
 }
